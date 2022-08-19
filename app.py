56e6778dd1dd7ffc2e4740ea49d89d887140ea40
Kapil <dave.kapil98@gmail.com>
2022-08-19 21:59:02 +0530

    Basic posting, explore, login, logout functions completed

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField, DateField, URLField
 from wtforms.validators import DataRequired, Length, ValidationError, EqualTo
 from flask_bcrypt import Bcrypt
 from datetime import datetime
 from config import Config
 
-#Configurations & Initializations
+# Configurations & Initializations
 #===============================================================================================================================================================================================================
 # Configure application
 app = Flask(__name__)
@@ -32,44 +32,55 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return users.query.get(int(user_id))
+    return User.query.get(int(user_id))
 #===============================================================================================================================================================================================================
 
 # Create database model
 #===============================================================================================================================================================================================================
 # Table to store user name and login credentials
-class users(db.Model, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), nullable = False, unique = True)
     name = db.Column(db.String(200), nullable = False)
     password = db.Column(db.String(80), nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy = True)
+    user_profile = db.relationship('User_Profile', backref='user_user', lazy = True)
+    followers = db.relationship('Follow', backref='following', lazy = True)
+        
+    def __repr__(self):
+        return f"User('{self.username}', '{self.name}', '{self.date_created}')"
     
 # Table to store the posts
-class posts(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(300), nullable = True)
     content_url = db.Column(db.String(), nullable = True)
-    likes = db.Column(db.Integer)
-    dislikes = db.Column(db.Integer)
+    likes = db.Column(db.Integer, default = 0)
+    dislikes = db.Column(db.Integer, default = 0)
     post_date = db.Column(db.DateTime, default = datetime.utcnow)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
+    def __repr__(self):
+        return f"Post('{self.content}', '{self.content_url}', '{self.likes}', '{self.dislikes}', '{self.post_date}')"    
 
 # Table to store profile information of users
-class user_profile(db.Model):
+class User_Profile(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     description = db.Column(db.String(300), nullable = True)
     profile_pic_url = db.Column(db.String(), nullable = True)
-    email = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=True)
     Birthdate = db.Column(db.Text, nullable = True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"User_Profile('{self.description}', '{self.profile_pic_url}', '{self.email}', '{self.Birthdate}', '{self.post_date}')"
     
 # Table to track follower list and following list
-class followers(db.Model):
+class Follow(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     follower_id = db.Column(db.Integer, nullable = False)
-    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    users_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 #=============================================================================================================================================================================================================== 
 
 # Ensure that respones are not cached
@@ -92,7 +103,7 @@ class RegisterForm(FlaskForm):
     submit = SubmitField('Register')    
     # Validate to see if the username is unique
     def validate_username(self, username):
-        existing_user_username = users.query.filter_by(
+        existing_user_username = User.query.filter_by(
             username = username.data).first()        
         if existing_user_username:
             raise ValidationError("That username already exists. Please choose a different one.")
@@ -103,15 +114,52 @@ class LoginForm(FlaskForm):
     password = PasswordField(validators=[DataRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    
+# Add Post Form
+class Add_PostForm(FlaskForm):
+    content = StringField(validators=[Length(max=300)], render_kw={"placeholder": "What's happening?"})
+    upload = URLField('Image URL', render_kw={"placeholder": "Image URL"})
+    submit = SubmitField('Post')
+    
+# New User Profile Form
+class MyProfileForm(FlaskForm):
+    description = StringField(validators=[DataRequired()], render_kw={"placeholder": "How would you describe yourself?"})
+    email = EmailField(validators=[DataRequired()], render_kw={"placeholder": 'Email'})
+    birthdate = DateField (validators=[DataRequired()], render_kw={"placeholder": 'Birthdate'})
+    submit = SubmitField("Update")
 #===============================================================================================================================================================================================================
 
 #All Routes
 #===============================================================================================================================================================================================================
 # Default route
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 @login_required
-def home():
-    return render_template("index.html")
+def index():
+    # Add new post
+    form = Add_PostForm()
+    if form.validate_on_submit():
+        
+        # Check if image is added or not
+        if form.upload.data:
+            url = form.upload.data
+        else:
+            url = None
+         
+        # Add the data of post to the database   
+        add_post = Post(content = form.content.data, author_id = current_user.id, content_url = url)
+        db.session.add(add_post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    
+    # Get all posts from the database
+    all_posts = Post.query.filter_by(author_id = current_user.id).all()
+    
+    # Sort the posts in LIFO order to see lastest posts on the top.
+    new_posts = []
+    for post in all_posts:
+        new_posts.insert(0,post)
+        
+    return render_template('index.html', form=form, posts = new_posts)
     
 #Register Route
 @app.route("/register", methods=["GET", "POST"])
@@ -119,10 +167,10 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = users(username = form.username.data, name = form.name.data, password = hashed_password)
+        new_user = User(username = form.username.data, name = form.name.data, password = hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('login'))    
+        return redirect(url_for('index'))    
     return render_template('register.html', form=form)
 
 # Login Route
@@ -130,11 +178,11 @@ def register():
 def login():
     form = LoginForm()    
     if form.validate_on_submit():
-        user = users.query.filter_by(username = form.username.data).first()
+        user = User.query.filter_by(username = form.username.data).first()
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
-                return redirect(url_for('home'))            
+                return redirect(url_for('index'))            
     return render_template('login.html', form=form)
 
 # Logout Route
@@ -143,6 +191,31 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+# New_Profile
+@app.route("/my_profile", methods = ["GET", "POST"])
+@login_required
+def my_profile():
+    form = MyProfileForm()
+    if form.validate_on_submit():
+        new_profile = User_Profile(description = form.description.data, email = form.email.data, Birthdate = form.birthdate.data, owner_id = current_user.id)
+        db.session.add(new_profile)
+        db.session.commit()
+        return redirect(url_for('index'))    
+    return render_template('myprofile.html', form=form)
+
+@app.route("/explore", methods = ["GET", "POST"])
+@login_required
+def explore():
+    form = Add_PostForm()
+    posts = Post.query.all()
+    # Sort the posts in LIFO order to see lastest posts on the top.
+    new_posts = []
+    for post in posts:
+        new_posts.insert(0,post)
+    
+    return render_template('explore.html', form=form, posts = new_posts)
+     
 #===============================================================================================================================================================================================================
 
 if __name__ == '__main__':
